369b353ab4f6e05170b9c7d62a8a0e76eadf4e7b
Gnillor <gump.zeng@gmail.com>
2017-02-24 13:36:29 +0800

    add example job and client script

---
+++ hadoop-deeplearning-project/YARN-TensorFlow/hadoop-yarn-applications-tensorflow/samples/between-graph/job.py
@@ -0,0 +1,47 @@
+# -*- coding:utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+import getopt
+import logging
+import os
+import subprocess
+import sys
+import thread
+import time
+
+import tensorflow as tf
+
+# input flags
+tf.app.flags.DEFINE_string("ps", "", "ps hosts")
+tf.app.flags.DEFINE_string("wk", "", "worker hosts")
+
+FLAGS = tf.app.flags.FLAGS
+
+ps_hosts = FLAGS.ps.split(',')
+worker_hosts = FLAGS.wk.split(',')
+
+cluster = tf.train.ClusterSpec({'ps': ps_hosts, 'worker': worker_hosts})
+
+
+def loop():
+    while 1:
+        time.sleep(2)
+        pass
+
+
+def cmd(i, target):
+    subprocess.call('python your-tensorflow-script.py --ps ' + FLAGS.ps + ' --wk ' + FLAGS.wk + ' --job_name="worker"' +
+                    ' --task_index=' + str(i) + ' --target=' + target, shell=True)
+
+
+def main(argv):
+
+    for i in range(len(worker_hosts)):
+        target = "grpc://" + worker_hosts[i]
+        cmd(i, target)
+
+    loop()
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
