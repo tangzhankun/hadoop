0ca9034a507a61fc100c8c948cdf938e9c064705
tangzhankun <tangzhankun@gmail.com>
2018-10-16 17:36:24 +0800

    add explanation for the version

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/java/org/apache/hadoop/yarn/server/nodemanager/api/deviceplugin/DeviceConstants.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/java/org/apache/hadoop/yarn/server/nodemanager/api/deviceplugin/DeviceConstants.java
@@ -21,6 +21,11 @@ package org.apache.hadoop.yarn.server.nodemanager.api.deviceplugin;
 public class DeviceConstants {
   /**
    * Follows the "majorVersion.minorVersion.patchVersion" convention
+   * Given a version number MAJOR.MINOR.PATCH, increment the:
+   *
+   * MAJOR version when you make incompatible API changes,
+   * MINOR version when you add functionality in a backwards-compatible manner, and
+   * PATCH version when you make backwards-compatible bug fixes.
    * */
   public static final String version = "0.1.0";
 }
