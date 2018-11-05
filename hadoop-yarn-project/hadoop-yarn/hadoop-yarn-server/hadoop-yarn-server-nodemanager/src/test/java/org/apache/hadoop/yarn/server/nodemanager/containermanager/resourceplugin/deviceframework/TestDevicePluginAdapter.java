7c2bd9bfa9c098cf279c4fa6bae37ac4cef80ea2
tangzhankun <tangzhankun@gmail.com>
2018-11-05 15:10:50 +0800

    add test case for DevicePluginAdapter

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/test/java/org/apache/hadoop/yarn/server/nodemanager/containermanager/resourceplugin/deviceframework/TestDevicePluginAdapter.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/test/java/org/apache/hadoop/yarn/server/nodemanager/containermanager/resourceplugin/deviceframework/TestDevicePluginAdapter.java
@@ -32,6 +32,7 @@ import org.apache.hadoop.yarn.server.nodemanager.containermanager.container.Cont
 import org.apache.hadoop.yarn.server.nodemanager.containermanager.container.ResourceMappings;
 import org.apache.hadoop.yarn.server.nodemanager.containermanager.linux.privileged.PrivilegedOperationExecutor;
 import org.apache.hadoop.yarn.server.nodemanager.containermanager.linux.resources.CGroupsHandler;
+import org.apache.hadoop.yarn.server.nodemanager.containermanager.linux.resources.ResourceHandlerException;
 import org.apache.hadoop.yarn.server.nodemanager.containermanager.resourceplugin.ResourcePluginManager;
 import org.apache.hadoop.yarn.server.nodemanager.containermanager.resourceplugin.deviceframework.examples.FakeDevicePlugin;
 import org.apache.hadoop.yarn.server.nodemanager.containermanager.runtime.ContainerRuntimeConstants;
@@ -50,6 +51,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 import static org.mockito.Matchers.isA;
 import static org.mockito.Mockito.*;
@@ -274,14 +276,14 @@ public class TestDevicePluginAdapter {
         mockCGroupsHandler, mockPrivilegedExecutor);
     adapter.getDeviceResourceHandler().bootstrap(conf);
 
-    // A container c1 requests 1 device
-    Container c1 = mockContainerWithDeviceRequest(0,
+    // A container c0 requests 1 device
+    Container c0 = mockContainerWithDeviceRequest(0,
         resourceName,
         1,false);
     // preStart
-    adapter.getDeviceResourceHandler().preStart(c1);
-    // ensure container1's resource has been persistent
-    verify(storeService).storeAssignedResources(c1, resourceName,
+    adapter.getDeviceResourceHandler().preStart(c0);
+    // ensure container1's resource is persistent
+    verify(storeService).storeAssignedResources(c0, resourceName,
         Arrays.asList(Device.Builder.newInstance()
             .setID(0)
             .setDevPath("/dev/hdwA0")
@@ -293,17 +295,148 @@ public class TestDevicePluginAdapter {
   }
 
   @Test
-  public void testRecoverDeviceSchedulerManagerState() {
-    // container1 request some resource
+  public void testRecoverDeviceSchedulerManagerState() throws IOException, YarnException {
+    NodeManager.NMContext context = mock(NodeManager.NMContext.class);
+    NMStateStoreService realStoreService = new NMMemoryStateStoreService();
+    NMStateStoreService storeService = spy(realStoreService);
+    when(context.getNMStateStore()).thenReturn(storeService);
+    doNothing().when(storeService).storeAssignedResources(isA(Container.class),
+        isA(String.class),
+        isA(ArrayList.class));
 
-    // ensure container1's resource has been persistent
+    // Init scheduler manager
+    DeviceSchedulerManager dsm = new DeviceSchedulerManager(context);
 
-    // NM restart say,
+    ResourcePluginManager rpm = mock(ResourcePluginManager.class);
+    when(rpm.getDeviceSchedulerManager()).thenReturn(dsm);
 
+    // Init an plugin
+    MyPlugin plugin = new MyPlugin();
+    MyPlugin spyPlugin = spy(plugin);
+    String resourceName = MyPlugin.resourceName;
+    // Init an adapter for the plugin
+    DevicePluginAdapter adapter = new DevicePluginAdapter(
+        resourceName,
+        spyPlugin, dsm);
+    // Bootstrap, adding device
+    adapter.initialize(context);
+    adapter.createResourceHandler(context,
+        mockCGroupsHandler, mockPrivilegedExecutor);
+    adapter.getDeviceResourceHandler().bootstrap(conf);
+    Assert.assertEquals(3,
+        dsm.getAllAllowedDevices().get(resourceName).size());
+    // mock NMStateStore
+    Device storedDevice = Device.Builder.newInstance()
+        .setID(0)
+        .setDevPath("/dev/hdwA0")
+        .setMajorNumber(256)
+        .setMinorNumber(0)
+        .setBusID("0000:80:00.0")
+        .setHealthy(true)
+        .build();
+    ConcurrentHashMap<ContainerId, Container> runningContainersMap
+        = new ConcurrentHashMap<>();
+    Container nmContainer = mock(Container.class);
+    ResourceMappings rmap = new ResourceMappings();
+    ResourceMappings.AssignedResources ar =
+        new ResourceMappings.AssignedResources();
+    ar.updateAssignedResources(
+        Arrays.asList(storedDevice));
+    rmap.addAssignedResources(resourceName, ar);
+    when(nmContainer.getResourceMappings()).thenReturn(rmap);
+    when(context.getContainers()).thenReturn(runningContainersMap);
+
+    // Test case 1. c0 get recovered. scheduler state restored
+    runningContainersMap.put(getContainerId(0), nmContainer);
+    adapter.getDeviceResourceHandler().reacquireContainer(
+        getContainerId(0));
+    Assert.assertEquals(3,
+        dsm.getAllAllowedDevices().get(resourceName).size());
+    Assert.assertEquals(1,
+        dsm.getAllUsedDevices().get(resourceName).size());
+    Assert.assertEquals(2,
+        dsm.getAvailableDevices(resourceName));
+    Map<Device, ContainerId> used = dsm.getAllUsedDevices().get(resourceName);
+    Assert.assertTrue(used.keySet().contains(storedDevice));
+
+    // Test case 2. c1 wants get recovered. But stored device is already allocated to c2
+    nmContainer = mock(Container.class);
+    rmap = new ResourceMappings();
+    ar = new ResourceMappings.AssignedResources();
+    ar.updateAssignedResources(
+        Arrays.asList(storedDevice));
+    rmap.addAssignedResources(resourceName, ar);
+    // already assigned to c1
+    runningContainersMap.put(getContainerId(2), nmContainer);
+    boolean caughtException = false;
+    try {
+      adapter.getDeviceResourceHandler().reacquireContainer(getContainerId(1));
+    } catch (ResourceHandlerException e) {
+      caughtException = true;
+    }
+    Assert.assertTrue(
+        "Should fail since requested device is assigned already",
+        caughtException);
+    // don't affect c0 allocation state
+    Assert.assertEquals(3,
+        dsm.getAllAllowedDevices().get(resourceName).size());
+    Assert.assertEquals(1,
+        dsm.getAllUsedDevices().get(resourceName).size());
+    Assert.assertEquals(2,
+        dsm.getAvailableDevices(resourceName));
+    used = dsm.getAllUsedDevices().get(resourceName);
+    Assert.assertTrue(used.keySet().contains(storedDevice));
   }
 
   @Test
-  public void testAssignedDeviceCleanupWhenStoreOpFails() {
+  public void testAssignedDeviceCleanupWhenStoreOpFails() throws IOException, YarnException {
+    NodeManager.NMContext context = mock(NodeManager.NMContext.class);
+    NMStateStoreService realStoreService = new NMMemoryStateStoreService();
+    NMStateStoreService storeService = spy(realStoreService);
+    when(context.getNMStateStore()).thenReturn(storeService);
+    doThrow(new IOException("Exception ...")).when(storeService).storeAssignedResources(isA(Container.class),
+        isA(String.class),
+        isA(ArrayList.class));
+
+    // Init scheduler manager
+    DeviceSchedulerManager dsm = new DeviceSchedulerManager(context);
+
+    ResourcePluginManager rpm = mock(ResourcePluginManager.class);
+    when(rpm.getDeviceSchedulerManager()).thenReturn(dsm);
+
+    // Init an plugin
+    MyPlugin plugin = new MyPlugin();
+    MyPlugin spyPlugin = spy(plugin);
+    String resourceName = MyPlugin.resourceName;
+    // Init an adapter for the plugin
+    DevicePluginAdapter adapter = new DevicePluginAdapter(
+        resourceName,
+        spyPlugin, dsm);
+    // Bootstrap, adding device
+    adapter.initialize(context);
+    adapter.createResourceHandler(context,
+        mockCGroupsHandler, mockPrivilegedExecutor);
+    adapter.getDeviceResourceHandler().bootstrap(conf);
+
+    // A container c0 requests 1 device
+    Container c0 = mockContainerWithDeviceRequest(0,
+        resourceName,
+        1,false);
+    // preStart
+    boolean exception = false;
+    try {
+      adapter.getDeviceResourceHandler().preStart(c0);
+    } catch (ResourceHandlerException e) {
+      exception = true;
+    }
+    Assert.assertTrue("Should throw exception in preStart", exception);
+    // no device assigned
+    Assert.assertEquals(3,
+        dsm.getAllAllowedDevices().get(resourceName).size());
+    Assert.assertEquals(0,
+        dsm.getAllUsedDevices().get(resourceName).size());
+    Assert.assertEquals(3,
+        dsm.getAvailableDevices(resourceName));
 
   }
 
